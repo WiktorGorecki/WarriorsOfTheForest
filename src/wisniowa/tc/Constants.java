1eb32945f08e5a6fe08d90a19b7650de838334ce
wiktorgorecki-portfolio <wiktor.gorecki05@gmail.com>
2023-04-17 14:46:53 +0200

    Added audio path constant to Constants.java.

--- src/wisniowa/tc/Constants.java
+++ src/wisniowa/tc/Constants.java
@@ -1,13 +1,21 @@
 package wisniowa.tc;
 
 public abstract class Constants {
+    //Dimensions
     public static final int CHARACTER_IMG_WIDTH = 40;
     public static final int CHARACTER_IMG_HEIGHT = 80;
     public static final int WINDOW_HEIGHT = 400;
     public static final int WINDOW_WIDTH = 400;
+
+    //Paths
     public static final String STATIC_FOLDER = "static/";
     public static final String IMAGE_FOLDER =
             STATIC_FOLDER + "images/";
+    public static final String AUDIO_FOLDER =
+            STATIC_FOLDER + "audio/";
+
+    //Math
+    //You probably don't need to remove or modify these declarations
     public static final int MAX_RIGHT_POSITION =
             WINDOW_WIDTH - CHARACTER_IMG_WIDTH;
     public static final int MAX_DOWN_POSITION =
